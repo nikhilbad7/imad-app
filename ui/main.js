c4f5bb3ac0acf223b2d332da97032309a6fa3534
nikhilbad7 <31141190+nikhilbad7@users.noreply.github.com>
2017-08-25 11:40:37 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ x.innerHTML="new value";
 var img=document.getElementById("img");
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
