0a2db04cb0b468b2bd1f9de0ce1b5364b270f245
nikhilbad7 <31141190+nikhilbad7@users.noreply.github.com>
2017-08-25 11:31:24 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var x=document.getElementById("x");
 x.innerHTML="new value";
+var img=document.getElementById("img");
+img.onclick=function(){
+    img.style.marginLeft="200px";
+};
