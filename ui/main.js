a9e6c6aef9c544f75d2d895b0197b82284740a26
nikhilbad7 <31141190+nikhilbad7@users.noreply.github.com>
2017-08-26 07:28:08 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,10 @@ request.send(null);
     var name=document.getElementById("name").value;
     var submit=document.getElementById("sub");
     submit.onclick=function(){
-        
+      var names=['name1','name2','name3','name4'];
+      var list='';
+      for(var i=0;i<names.length;i++){
+          list+="<li>" + names[i] +"</li>";
+      }  
+      var ui=document.getElementById("nameslist");
     };
