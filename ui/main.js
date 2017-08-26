6edce8128d596615b4333349f94033a123c18d1c
nikhilbad7 <31141190+nikhilbad7@users.noreply.github.com>
2017-08-26 08:19:25 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,10 +16,21 @@ request.send(null);
     var name=document.getElementById("name").value;
     var submit=document.getElementById("sub");
     submit.onclick=function(){
-      var names=['name1','name2','name3','name4'];
+            var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           if(request.status===200){
+               
+      var names=request.responseText;
+      names=JSON.parse(names);
       var list='';
       for(var i=0;i<names.length;i++){
           list +="<li>" + names[i] +"</li>";}
       var ui=document.getElementById("namelist");
       ui.innerHTML=list;
+           }
+       }
+    };
+    request.open("GET","http://nikhilbadani.imad.hasura-app.io/submit-name?name="+ name,true);
+request.send(null);
     };
