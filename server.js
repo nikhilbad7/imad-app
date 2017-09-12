19efe42d46929b76e926e6328ed9ec8c3331c7bf
nikhilbad7 <31141190+nikhilbad7@users.noreply.github.com>
2017-09-12 10:03:29 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,11 +106,11 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-function hash(input,salt){
+function hash (input,salt){
   var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
   return hashed.toString('hex');
 }
-app.get('/hash/"input:',function(req,res){
+app.get('/hash/input:',function(req,res){
     var hashedString=hash(req.params.input,'randomstring');
     res.send(hashedString);
     
